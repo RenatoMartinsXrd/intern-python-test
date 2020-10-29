fa3a96f046eb47c452a38d49548213e7929d7d09
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2020-10-29 09:13:06 -0300

    Update convert_json_to_csv.py

--- convert_json_to_csv.py
+++ convert_json_to_csv.py
@@ -34,18 +34,6 @@ writeCSV(pathCsv,data,"|")
 
 
 
-	
-
-
-# with open(pathCsv) as csv_file:
-    
-#     csv_reader = csv.reader(csv_file, delimiter='|')
-
-
-
-#     for row in csv_reader:
-#         print( row[0] + ', ' + row[1] + ', ' + row[2] )
-
 
 
 
